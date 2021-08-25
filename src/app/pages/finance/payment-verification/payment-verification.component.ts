476941bfceb07741a1e226b5f44df2fb15907cef
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-08-25 10:56:27 -0400

    feat: crear payment verification page

---
+++ src/app/pages/finance/payment-verification/payment-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-verification',
+  templateUrl: './payment-verification.component.html',
+  styleUrls: ['./payment-verification.component.scss']
+})
+export class PaymentVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
