b8d6561da161ff087555b4096133f0f676484499
Sabas Vega <sabascarloseduardo@gmail.com>
2021-08-31 15:08:18 -0500

    Added env production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_URL : "https://api.descomplicate.dev.cronapis.com/api/"
+  API_BASE_URL : "https://api.descomplicate.cronapis.com/api/"
+  // API_BASE_URL : "https://api.descomplicate.dev.cronapis.com/api/"
 };
