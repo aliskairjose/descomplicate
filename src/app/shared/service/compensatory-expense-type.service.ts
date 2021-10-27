44ca7dd721ff94b5b9bb199d063b27a00d07b268
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-10-27 11:50:57 -0400

    feat: service compensatory

---
+++ src/app/shared/service/compensatory-expense-type.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpserviceService } from './httpservice.service';
+import { Observable } from 'rxjs';
+import { BaseResponse } from '../interfaces/response';
+import { CompensatoryExpenseType } from '../interfaces/compenatory-expense-type';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class CompensatoryExpenseTypeService {
+
+  constructor(
+    private http: HttpserviceService
+  ) { }
+
+  /**
+   * @description Lista los gastos compensatorios
+   * @returns 
+   */
+  list(): Observable<BaseResponse<CompensatoryExpenseType[]>> {
+    return this.http.get( `compensatory-expense-types` );
+  }
+
+  /**
+   * @description Crea un nuevo recurso
+   * @param data 
+   * @returns 
+   */
+  store( data: any ): Observable<any> {
+    return this.http.get( `compensatory-expense-types`, data );
+  }
+
+  /**
+   * @description Actualiza la data del recurso
+   * @param id Id del recurso a actualizar
+   * @param data Datos del recurso a actualizar
+   * @returns 
+   */
+  update( id: number, data: any ): Observable<BaseResponse<CompensatoryExpenseType>> {
+    return this.http.get( `compensatory-expense-types/${id}` );
+  }
+
+  /**
+   * @description Busca un recurso por el id
+   * @param id Id del recurso
+   * @returns 
+   */
+  show( id: number ): Observable<Observable<BaseResponse<CompensatoryExpenseType>>> {
+    return this.http.get( `compensatory-expense-types/${id}` );
+  }
+
+  /**
+   * @description Eliima un recurso por el id suministrado
+   * @param id Id del recurso
+   * @returns 
+   */
+  delete( id: number ): Observable<Observable<BaseResponse<CompensatoryExpenseType>>> {
+    return this.http.delete( `compensatory-expense-types/${id}` );
+  }
+
+
+
+}
