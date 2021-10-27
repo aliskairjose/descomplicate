b89e7723ce24c51a421288cdf200233a77fc8951
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-10-27 10:36:59 -0400

    refactor

--- src/app/pages/auth/login-admin/login-admin.component.ts
+++ src/app/pages/auth/login-admin/login-admin.component.ts
@@ -4,56 +4,45 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/shared/service/storage.service';
 import { AuthService } from 'src/app/shared/service/auth.service';
 import { TOKEN, USER } from 'src/app/shared/constants/global-constans';
-@Component({
+@Component( {
   selector: 'app-login-admin',
   templateUrl: './login-admin.component.html',
-  styleUrls: ['./login-admin.component.scss']
-})
+  styleUrls: [ './login-admin.component.scss' ]
+} )
 export class LoginAdminComponent implements OnInit {
-  fieldTextType : boolean = false;
-  submitted :boolean = false;
-  FormLogin = new FormGroup({
-    email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('',[Validators.required,Validators.minLength( 8 )]),
-  });
+  fieldTextType: boolean = false;
+  submitted: boolean = false;
+  FormLogin = new FormGroup( {
+    email: new FormControl( '', [ Validators.required, Validators.email ] ),
+    password: new FormControl( '', [ Validators.required, Validators.minLength( 8 ) ] ),
+  } );
   constructor(
     private router: Router,
     private auth: AuthService,
-		private storage: StorageService,
-		private formBuilder: FormBuilder,
-  ) { 
-  
+    private storage: StorageService,
+    private formBuilder: FormBuilder,
+  ) {
+
   }
 
   get f() { return this.FormLogin.controls; }
-  
+
   ngOnInit(): void {
   }
 
-
-  toggleFieldTextType(){
-    this.fieldTextType = !this.fieldTextType;
-  }
-
-  onSubmit(){
+  onSubmit() {
     this.submitted = true;
-    if(this.FormLogin.valid){
+    if ( this.FormLogin.valid ) {
       this.auth.login( 'admin', this.FormLogin.value ).subscribe( response => {
-				this.auth.isAuthSubject( true );
-				this.auth.userSubject( response.user );
-				this.storage.setItem( TOKEN, response.data );
-				this.storage.setItem( USER, response.user );
-				this.router.navigateByUrl( 'pages/dashboard' );
-    
-				// Mostrar mensaje de bienvenida
-			},
-      error =>{
-        // console.log(error);
+        this.auth.isAuthSubject( true );
+        this.auth.userSubject( response.user );
+        this.storage.setItem( TOKEN, response.data );
+        this.storage.setItem( USER, response.user );
+        this.router.navigateByUrl( 'pages/dashboard' );
+
       } );
     }
 
   }
-
-
 
 }
